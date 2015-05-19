3cd2b5ae6df1677ffb9b1f1d6aa05f7cad9ba9ae
Llama Glama <lamah83@gmail.com>
2015-05-19 22:35:35 +0200

    Added Transition type

---
+++ test/Objects/Transition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using OBS;
+
+namespace test
+{
+	public class Transition : ObsSource
+	{
+		public Transition(string id, string name) : base(ObsSourceType.Transition, id, name)
+		{
+		}
+
+		public Transition(string id, string name, ObsData settings) : base(ObsSourceType.Transition, id, name, settings)
+		{
+		}
+
+		public Transition(IntPtr instance) : base(instance)
+		{
+		}
+	}
+}
